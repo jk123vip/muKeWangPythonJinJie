23c9777bd6fb57cf891eb71b57ba77d85331f157
jk123vip <jk123vip@gmail.com>
2015-03-18 20:19:59 +0800

    4-2 定义类并创建类型

---
+++ 4-2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 20:16:28 2015
+
+@author: JK
+"""
+
+"""
+4-2 定义类并创建实例
+
+请练习定义Person类，并创建出两个实例，打印实例，再比较两个实例是否相等。
+"""
+
+class Person():
+    pass
+
+xiaoming = Person()
+xiaohong = Person()
+
+print xiaoming
+print xiaohong
+print xiaoming == xiaohong
